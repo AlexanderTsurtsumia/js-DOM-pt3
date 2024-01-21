c98071606348306d6db0e0ecbe591c18688d04e4
AlexanderTsurtsumia <127110773+AlexanderTsurtsumia@users.noreply.github.com>
2024-01-21 15:19:05 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-var div = document.getElementsByClassName('c')
-for (let i = 0; i < div.length; i++) {
-    const element = div[i];
-    div[i].style.height = "200px";
-    div[i].style.backgroundColor = "red";
-    div[i].style.width = "200px";
-    div[i].innerHTML = ( i + 1 );
+var li = document.getElementsByTagName('li')
+for (let i = 0; i < li.length; i++) {
+    const element = li[i];  
+    li[i].innerHTML = ( i + 1 );
 }
